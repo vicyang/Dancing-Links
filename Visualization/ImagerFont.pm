85ecca03b7181fe17664719a267ea9c79ac0d990
Vulkan <523066680@163.com>
2018-11-21 09:29:05 +0800

    标签文字画布使用和block一样尺寸的大小。居中显示

--- Visualization/ImagerFont.pm
+++ Visualization/ImagerFont.pm
@@ -1,28 +1,26 @@
 package ImagerFont;
 
-
 use utf8;
 use Encode;
 use Imager;
 use Encode;
 use OpenGL;
 
+our $h = 30; # 画布尺寸
+our $w = 30;
 our $SIZE = 30;
 our $font = Imager::Font->new(file  => encode('gbk', 'C:/windows/fonts/arialbd.ttf'), #STXINGKA.TTF
                               size  => $SIZE );
+
 our $bbox = $font->bounding_box(string=>"");
 
 sub get_text_map
 {
-    our ($font, $SIZE);
+    our ($font, $SIZE, $w, $h);
     my ( $char, $ref ) = @_;
 
     my $bbox = $font->bounding_box( string => $char );
-    my $img = Imager->new(xsize=>$bbox->display_width, 
-                          ysize=>$bbox->text_height, channels=>4);
-
-    my $h = $img->getheight();
-    my $w = $img->getwidth();
+    my $img = Imager->new(xsize=>$w, ysize=>$h, channels=>4);
 
     # # 填充画布背景色
     # $img->box(xmin => 0, ymin => 0, xmax => $w, ymax => $h,
@@ -31,12 +29,12 @@ sub get_text_map
     $img->align_string(
                font  => $font,
                text  => $char,
-               x     => 0,
-               y     => $h,
+               x     => $w/2.0,
+               y     => $h/2.0,
                size  => $SIZE,
                color => 'white',
                aa    => 1,     # anti-alias
-               valign => 'bottom',
+               valign => 'center', halign => 'center', 
             );
 
     $ref->{h} = $h, $ref->{w} = $w;
